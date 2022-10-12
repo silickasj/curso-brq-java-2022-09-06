9a8d52f9d6390f0ae9f751636e8033cf312ba8d0
Jonas Paulo de Oliveira Silickas <jsilikas@brq.com>
2022-10-12 19:08:47 -0300

    terceira alteracao pgm 02

--- P1EX02.java
+++ P1EX02.java
@@ -1,11 +1,11 @@
 public class P1EX02 {
 	public static void main(String[] args) {
 		double cavalos;
-		double m = 2000.0;
-		double h = 200.0;
-		double t = 20.0;
+		double m = 3000.0;
+		double h = 300.0;
+		double t = 30.0;
 		
-		//segunda alteracao
+		//terceira alteracao teste git
 		
 		cavalos = m * h / t / 745.6999;
 		
